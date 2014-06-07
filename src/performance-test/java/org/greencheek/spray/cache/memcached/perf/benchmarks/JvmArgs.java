d12ec8a477c2caa48cbfa1dc71a30b82f09e8d17
dominictootell <dominic.tootell@gmail.com>
2014-06-07 11:28:15 +0100

    add jfr args

--- src/performance-test/java/org/greencheek/spray/cache/memcached/perf/benchmarks/JvmArgs.java
+++ src/performance-test/java/org/greencheek/spray/cache/memcached/perf/benchmarks/JvmArgs.java
@@ -4,10 +4,10 @@ package org.greencheek.spray.cache.memcached.perf.benchmarks;
  * Created by dominictootell on 07/06/2014.
  */
 public class JvmArgs {
-    private final static String JFR_JVM_ARGS ="-server -XX:+UnlockCommercialFeatures -XX:+FlightRecorder -XX:FlightRecorderOptions=defaultrecording=true,settings=profile,filename=target/recording.jfr";
-    private final static String JVM_ARGS ="-server";
+    private final static String[] JFR_JVM_ARGS = new String[]{"-server","-XX:+UnlockCommercialFeatures","-XX:+FlightRecorder","-XX:FlightRecorderOptions=defaultrecording=true,settings=profile,filename=target/recording.jfr"};
+    private final static String[] JVM_ARGS = new String[]{"-server"};
 
-    public static String getJvmArgs() {
+    public static String[] getJvmArgs() {
         String profile = System.getProperty("enablejfr","true");
         boolean flightRecorderEnabled;
         if(profile==null || profile.trim().length()==0) {
