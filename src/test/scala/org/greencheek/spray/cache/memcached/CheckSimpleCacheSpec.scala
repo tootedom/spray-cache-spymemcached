df3bd26786ccf6b48e950a2580d6170faadc69c7
dominictootell <dominic.tootell@gmail.com>
2014-06-13 00:01:26 +0100

    update duration checking logic.  precalc get and set millis

---
+++ src/test/scala/org/greencheek/spray/cache/memcached/CheckSimpleCacheSpec.scala
@@ -0,0 +1,56 @@
+package org.greencheek.spray.cache.memcached
+
+import scala.concurrent._
+import org.specs2.mutable.Specification
+import akka.actor.ActorSystem
+import org.greencheek.util.memcached.WithMemcached
+import java.util.concurrent.{TimeUnit, CountDownLatch}
+import akka.actor.ActorSystem
+import scala.concurrent.duration._
+import org.specs2.mutable.Specification
+import spray.util._
+import scala.concurrent._
+import ExecutionContext.Implicits.global
+import org.greencheek.util.memcached.WithMemcached
+import net.spy.memcached.ConnectionFactoryBuilder.Protocol
+import org.greencheek.spray.cache.memcached.keyhashing.{NoKeyHash, KeyHashType}
+import net.spy.memcached.HashAlgorithm
+
+/**
+ * Created by dominictootell on 11/06/2014.
+ */
+class CheckSimpleCacheSpec extends Specification {
+  implicit val system = ActorSystem()
+
+  val memcachedContext = WithMemcached(false)
+
+  "A Memcached cache" >> {
+
+
+    "store and wait on same future" in memcachedContext {
+      val hosts = "localhost:"+memcachedContext.memcached.port
+
+      val cache = new MemcachedCache[String] ( memcachedHosts = hosts, protocol = Protocol.TEXT,
+        timeToLive = Duration(5,TimeUnit.SECONDS),waitForMemcachedSet = true)
+
+      val option1 = cache("20")(future {
+        try {
+          Thread.sleep(1000)
+        } catch {
+          case e: Exception => {
+
+          }
+        }
+        "hello"
+      })
+
+
+      cache.get("20").get.await == "hello"
+
+      option1.await == "hello"
+
+
+      true
+    }
+  }
+}
